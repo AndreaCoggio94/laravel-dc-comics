a7c3f2cd4ad2493cbee0a4fbffa59021f8ab7a5c
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-10-18 17:16:59 +0200

    added future link to detailed comic

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,4 +2,6 @@
 
 @section('main-content')
     @dump($comics)
+{{-- da inserire all'interno di un singolo comic --}}
+    {{-- <a href="{{ route('comics.show', $comic)}}">dettagli comic</a> --}}
 @endsection

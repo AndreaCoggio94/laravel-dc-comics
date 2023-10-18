70cff495a2fd0bddbaa905cdf31df33d395bd6e5
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-10-18 19:53:47 +0200

    added comics database and starting seeder

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $_comics
+
+        $comic = new Comic();
+
+
+    }
+}

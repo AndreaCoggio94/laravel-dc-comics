98eaa9fd6b5c0af81bb87f636b8722740f5ef9b9
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-10-19 15:57:06 +0200

    added delete button to comics>show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,11 +7,18 @@
             <div class="card-body">
               <h5 class="card-title">{{$comic->title}}</h5>
               <p class="card-text">{{$comic->description}}</p>
-              <a href="{{ route('comics.index')}}" class="btn btn-primary">Return to comics</a>
-              <a href="{{ route('comics.edit', $comic)}}" class="btn btn-warning">Edit this comic</a>
-              
+              <div class="d-flex">
+
+                <a href="{{ route('comics.index')}}" class="btn btn-primary mx-1">Return to comics</a>
+                <a href="{{ route('comics.edit', $comic)}}" class="btn btn-warning mx-1">Edit this comic</a>
+                <form action="{{route('comics.destroy', $comic)}}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <input class="btn btn-danger mx-1" type="submit" value="delete">
+                </form>
+              </div>
             </div>
-          </div>
+            </div>
     </div>
     
 @endsection
